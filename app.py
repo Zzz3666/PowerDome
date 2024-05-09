d4018bf7db31a3b4f7ae30ab8eb5bdbc1c066462
Zhou Yan <zhouyan_90@live.cn>
2024-05-09 23:07:01 +0800

    暂时保存数据

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 """ app 入口"""
-#import streamlit as st
+import streamlit as st
+import graphviz as gv
 from app.page_header import page_header
 
 page_header(title="主页")
+
+digital_power_supply_design_flow = gv.Digraph()
+st.graphviz_chart(digital_power_supply_design_flow)
