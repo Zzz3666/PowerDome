ae99060f01711b8d1f94ce974df9478e72066de7
aworkshop <zhouyan_90@live.cn>
2024-05-10 11:58:25 +0800

    生成电源设计流程框图

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 """ app 入口"""
+
 import streamlit as st
-import graphviz as gz # type: ignore
+import graphviz as gz  # type: ignore
 from app.page_header import page_header
 
 page_header(title="主页")
 
+digital_power_supply_design_flow_data = [
+    "需求分析",
+    "拓扑选择",
+    "系统设计",
+    "硬件设计",
+    "软件开发",
+    "仿真分析",
+    "原型制作",
+    "测试调试",
+    "优化改进",
+    "认证标准",
+    "生产准备",
+    "质量控制",
+]
+
 digital_power_supply_design_flow = gz.Digraph()
-digital_power_supply_design_flow.edge("1", "2")
-digital_power_supply_design_flow.edge("2", "3")
+for index in range(len(digital_power_supply_design_flow_data) - 1):
+    digital_power_supply_design_flow.edge(
+        digital_power_supply_design_flow_data[index],
+        digital_power_supply_design_flow_data[index + 1],
+    )
 st.graphviz_chart(digital_power_supply_design_flow)
