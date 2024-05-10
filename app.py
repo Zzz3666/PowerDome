f2944c0c4531f2daa447a8b90947316a171c9f9a
aworkshop <zhouyan_90@live.cn>
2024-05-10 09:37:47 +0800

    排除 graphviz 数据检查

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 """ app 入口"""
 import streamlit as st
-import graphviz as gv
+import graphviz as gz # type: ignore
 from app.page_header import page_header
 
 page_header(title="主页")
 
-digital_power_supply_design_flow = gv.Digraph()
+digital_power_supply_design_flow = gz.Digraph()
+digital_power_supply_design_flow.edge("1", "2")
+digital_power_supply_design_flow.edge("2", "3")
 st.graphviz_chart(digital_power_supply_design_flow)
