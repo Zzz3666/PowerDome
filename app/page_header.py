ff957cfdefd54d43e70ffa87290ab978e719a47e
aworkshop <zhouyan_90@live.cn>
2024-05-10 16:37:18 +0800

    多页面功能添加

--- app/page_header.py
+++ app/page_header.py
@@ -1,7 +1,19 @@
 """ 页面标题显示及必备模块 """
 
+import os
 import streamlit as st
+import pandas as pd
+
+
 def page_header(title):
-    """ 页面顶端显示 """
+    """页面顶端显示"""
     st.title(title)
     st.divider()
+
+    page_root_path = "pages/"
+
+    if os.path.exists("data/page.csv"):
+        page_data = pd.read_csv("data/page.csv")
+        for i in range(len(page_data)):
+            page_path = page_root_path + str(page_data.iloc[i, 0]) + ".py"
+            st.sidebar.page_link(page_path, label=str(page_data.iloc[i, 1]))
