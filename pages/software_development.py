19e2153c3cc237e11c28bf1093a87d43212b8ff1
aworkshop <zhouyan_90@live.cn>
2024-05-11 13:42:58 +0800

    更新软件开发文档

--- pages/software_development.py
+++ pages/software_development.py
@@ -1,8 +1,15 @@
 """ 软件开发页面 """
+
 import streamlit as st
 from app.page_header import page_header
 
 page_header(title="软件开发")
+
+st.header("Petalinux 编译")
+st.divider()
+
+st.header("Python 编译")
+st.divider()
 
 st.header("WEB APP 开发")
 st.divider()
