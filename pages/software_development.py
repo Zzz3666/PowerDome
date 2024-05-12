e55dcc1178f1a61101366b0fdd8eabd71b1bf588
Zhou Yan <zhouyan_90@live.cn>
2024-05-12 19:21:33 +0800

    更新部署环境

--- pages/software_development.py
+++ pages/software_development.py
@@ -5,5 +5,17 @@ from app.page_header import page_header
 
 page_header(title="软件开发")
 
-st.header("Petalinux 配置")
+st.header("开发环境")
+st.divider()
 
+st.header("虚拟机")
+st.divider()
+st.write("VMware Workstation 17.5.1")
+
+st.header("宿主机")
+st.divider()
+st.write("Deepin 20.9")
+st.write("Windows 11 Home 21H2")
+
+st.header("Petalinux 配置")
+st.divider()
